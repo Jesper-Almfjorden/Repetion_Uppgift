ee381b567b02d934a7a1280032a392fbf61979e1
Jesper Almfjorden <almfjorden16@gmail.com>
2017-01-23 13:44:16 +0100

    End of lesson

---
+++ PRRPRR01_Repetition_01/Assets/Uppgift1_variabel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Uppgift1_variabel : MonoBehaviour {
+
+    int a = 10;
+    int b = 20;
+    int c = 0;
+
+	// Use this for initialization
+	void Start ()
+    {      
+        Debug.Log(a);
+        Debug.Log(b);       
+
+        c = a;
+        a = b;
+        b = c;
+              
+        Debug.Log(a);
+        Debug.Log(b);
+
+
+	}
+	
+}
---
+++ PRRPRR01_Repetition_01/Assets/Uppgift2.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Uppgift2 : MonoBehaviour {
+
+    int a = 1;
+    int b = 2;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (a < b) ;
+        print(2);
+	}
+	
+}
